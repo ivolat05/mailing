d20dc14a5aee6bc61be1c2b7634aed5319b40668
Andrei Labadaev <andrei-labadaev@mail.ru>
2021-08-22 13:25:03 +0300

    uppdate 22.08.2021 13.26

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
+$(function () {
+    $('.menu__burger').click((event) => {
+        $('.menu').toggleClass('menu__active'),
+            $('.menu__burger').toggleClass('menu__burger-active'),
+            $('body').toggleClass('look')
+    });
 
 });

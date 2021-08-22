4f15ba729f9ccf7703b51c42066361edda8e7c21
Andrei Labadaev <andrei-labadaev@mail.ru>
2021-08-22 19:56:03 +0300

    update 22.03.2021 19.55

--- js/main.js
+++ js/main.js
@@ -5,4 +5,27 @@ $(function () {
             $('body').toggleClass('look')
     });
 
+    const navBtn = document.querySelectorAll('.tarif__list-inner');
+    const tabItem = document.querySelectorAll('.tarif__box');
+    navBtn.forEach((item) => {
+        item.addEventListener('click', function () {
+            let activeBtn = item;
+            let activeId = activeBtn.getAttribute('data-tab');
+            let activeTab = document.querySelector(activeId);
+
+
+            if (!activeBtn.classList.contains('tarif__inner-active')) {
+                navBtn.forEach(function (item) {
+                    item.classList.remove('tarif__inner-active');
+                });
+                tabItem.forEach(function (item) {
+                    item.classList.remove('tarif__box-active');
+                });
+
+                activeBtn.classList.add('tarif__inner-active');
+                activeTab.classList.add('tarif__box-active');
+            }
+        });
+    });
+
 });

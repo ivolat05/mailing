7edbad8e94fc1e165280d6f47b902602d11767ba
Andrei Labadaev <andrei-labadaev@mail.ru>
2021-08-23 21:47:46 +0300

    update 23.08.2021 21.47

--- js/main.js
+++ js/main.js
@@ -99,4 +99,14 @@ $(function () {
         autoplaySpeed: 2000,
         arrows: false
     });
+
+    $('.experience__wrapp').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false
+    });
 });

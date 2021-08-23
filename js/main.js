31465ee952e7d46a7d587a93da7e61f8200c858f
Andrei Labadaev <andrei-labadaev@mail.ru>
2021-08-23 15:09:25 +0300

    update 23.08.2021 15.00

--- js/main.js
+++ js/main.js
@@ -74,4 +74,19 @@ $(function () {
         });
     });
 
+    const acc = document.getElementsByClassName("accordion-title");
+    let i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
+
 });
